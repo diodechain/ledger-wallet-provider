fab6974361f2fe5c675e691e17da8735e92c6658
Denís Graña <denisgranha@gmail.com>
2017-03-20 18:00:05 +0100

    browser version, returns web3 object, not provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import HookedWalletSubprovider from "web3-provider-engine/subproviders/hooked-wallet.js";
 import LedgerWallet from "./LedgerWallet";
 
-
-export default async function () {
+export default async function (rpcUrl) {
     const ledger = new LedgerWallet();
     await ledger.init();
     const LedgerWalletSubprovider = new HookedWalletSubprovider(ledger);
@@ -11,6 +10,18 @@ export default async function () {
     // before adding the LedgerWalletSubprovider to a providerEngine instance.
     LedgerWalletSubprovider.isSupported = ledger.isU2FSupported;
     LedgerWalletSubprovider.ledger = ledger;
+    var Web3 = require('web3');
+    var ProviderEngine = require('web3-provider-engine');
+    var RpcSubprovider = require('web3-provider-engine/subproviders/rpc');
 
-    return LedgerWalletSubprovider;
+    var engine = new ProviderEngine();
+    var web3 = new Web3(engine);
+    engine.addProvider(LedgerWalletSubprovider);
+    engine.addProvider(new RpcSubprovider(
+      {
+        rpcUrl: rpcUrl || "https://kovan.infura.io:443"
+      }
+    ));
+    engine.start();
+    return web3;
 };
